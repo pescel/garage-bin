cc987f8e70347c69fef73725d59fe01c86d33e6e
pescel <28l.pesce@gmail.com>
2017-04-13 15:35:25 -0600

    sorting and counting functions

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+// Store items in global array so I dont have to constantly query the dom
+let garageItems = [];
 
 $(document).ready(()=>{
   //addGarageItems()
@@ -13,14 +15,20 @@ fetchGarageItems = () => {
   fetch('/api/items')
   .then((response) => { return response.json() })
   .then((items) => {
-    renderGarageItems(items)
+    garageItems = items
+    renderGarageItems()
+    updateCount()
   })
 }
 
-renderGarageItems = (items) => {
-  items.map((item) => {
-    $('.items-list').append(`<li> Name: ${item.name} <br/> Reason:  ${item.reason} <br/> Cleanliness: ${item.cleanliness}</li>`)
+renderGarageItems = () => {
+  garageItems.map((item) => {
+    $('.items-list').append(`<li>${item.name}</li>`)
   })
+}
+
+clearGarageItems = () => {
+  $('.items-list').empty()
 }
 
 $('.add-item-btn').on('click', (e) => {
@@ -31,7 +39,6 @@ $('.add-item-btn').on('click', (e) => {
     reason: $('.storage-reason-input').val(),
     cleanliness: $('.cleanliness-dropdown').val()
   }
-
   postNewItem(newItem)
 })
 
@@ -46,13 +53,45 @@ postNewItem = (newItem) => {
   })
   .then((response) => { return response.json() })
   .then((item) => {
-    $('.items-list').append(`<li> Name: ${item.name} <br/> Reason:  ${item.reason} <br/> Cleanliness: ${item.cleanliness}</li>`)
+    $('.items-list').append(`<li>${item.name}</li>`)
+    garageItems.push(item)
+    updateCount()
   })
 
   clearInputs()
 }
 
 clearInputs = () => {
-  $('.name-input').val("")
-  $('.storage-reason-input').val("")
+  $('.name-input').val('')
+  $('.storage-reason-input').val('')
+}
+
+$('.sort-btn').on('click', () => {
+  sortItems()
+})
+
+sortItems = () => {
+  // Use .slice() to copy the array so the original is not sorted
+  sortedItems = garageItems.slice()
+  sortedItems.sort((a, b) => {
+    return a.name < b.name
+  })
+  // If the first item is the same, reverse the order to get A-Z, Z-A effect
+  if (garageItems[0].name === sortedItems[0].name){
+    sortedItems.reverse()
+  }
+
+  garageItems = sortedItems
+  clearGarageItems()
+  renderGarageItems()
+}
+
+updateCount = () => {
+  let sparklingItems = garageItems.filter((item)=>{return item.cleanliness == "sparkling"})
+  let dustyItems = garageItems.filter((item)=>{return item.cleanliness == "dusty"})
+  let rancidItems = garageItems.filter((item)=>{return item.cleanliness == "rancid"})
+  $('.total-count').text(garageItems.length)
+  $('.sparkling-count').text(sparklingItems.length)
+  $('.dusty-count').text(dustyItems.length)
+  $('.rancid-count').text(rancidItems.length)
 }
