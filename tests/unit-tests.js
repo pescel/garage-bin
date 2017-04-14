e7e507816e8d8f72b6ae7ffff50afc1204474efd
pescel <28l.pesce@gmail.com>
2017-04-13 19:06:54 -0600

    Attemp unit test

--- tests/unit-tests.js
+++ tests/unit-tests.js
@@ -2,11 +2,15 @@ const chai = require('chai')
 const expect = chai.expect
 const { sortItems } from '../public/index'
 
-
-
 describe('Index', () => {
 
   it('sortItems should sort items by name', () => {
-    expect('sortItems').isFunction()
-  })
-})
+    let items = {
+      'bike',
+      'rug',
+      'car'
+    }
+
+    expect(sortItems(items)).to.equal({'bike', 'car', 'rug'})
+  });
+});
